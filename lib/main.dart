9031c13a7454b3b6a1857c9d46312fb2a8968f1c
unknown <ardaaskindm@gmail.com>
2025-02-23 23:22:39 +0300

    last updates

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SplashScreen(), // SplashScreen başlangıç ekranı
+      home: const SplashScreen(), // SplashScreen ana sayfa olarak atanıyor
     );
   }
 }
@@ -44,26 +44,31 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    _checkAuthState();
+    _checkCurrentUser();
   }
 
-  void _checkAuthState() async {
-    await Future.delayed(const Duration(seconds: 2)); // Gecikme ekleyerek Firebase'in yüklenmesini bekle
-    FirebaseAuth.instance.authStateChanges().listen((User? user) {
-      if (mounted) {
-        if (user != null) {
+  void _checkCurrentUser() async {
+    await Future.delayed(const Duration(seconds: 4));
+    try {
+      User? user = FirebaseAuth.instance.currentUser;
+      if (user != null && !user.isAnonymous) {
+        if (mounted) {
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => const MainPage()),
           );
-        } else {
+        }
+      } else {
+        if (mounted) {
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => const LoginScreen()),
           );
         }
       }
-    });
+    } catch (e) {
+      print("Firebase Auth error: $e");
+    }
   }
 
   @override
@@ -71,24 +76,26 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Stack(
         children: [
+          // Mavi arka plan
           Positioned.fill(
             child: Container(
-              color: Colors.blue, // Arka plan rengi
+              color: Colors.blue, // Mavi arka plan rengi
             ),
           ),
+          // Yükleniyor spinner'ı ve yazı
           Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
                 CircularProgressIndicator(
-                  color: Colors.white,
+                  color: Colors.white, // Spinner rengi beyaz
                 ),
                 const SizedBox(height: 20),
                 const Text(
                   "Oturum Yükleniyor...",
                   style: TextStyle(
                     fontSize: 18,
-                    color: Colors.white,
+                    color: Colors.white, // Yazı rengi beyaz
                     fontWeight: FontWeight.bold,
                   ),
                 ),
